37931178089504139a7e15a9ac318e5663270948
User <User@DESKTOP-34G779U>
2022-05-29 16:56:40 +0530

    first commit

---
+++ corejava/src/xyz/test.java
@@ -0,0 +1,13 @@
+package xyz;
+import abc.Addition;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Addition ob1=new Addition();
+		System.out.println(ob1.addtwonumbers(10,211));
+
+	}
+
+}
