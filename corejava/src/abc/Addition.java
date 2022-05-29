f4056fddaa8db95f35d694658179302e1eacf7d2
User <User@DESKTOP-34G779U>
2022-05-29 16:53:56 +0530

    first commit

---
+++ corejava/src/abc/Addition.java
@@ -0,0 +1,17 @@
+package abc;
+
+public class Addition {
+	public int addtwonumbers(int a,int b) {
+		return a+b;
+}
+class b
+{}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Addition ob1=new Addition();
+		ob1.addtwonumbers(2,3);
+
+	}
+
+}
