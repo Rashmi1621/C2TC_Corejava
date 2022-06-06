a480e2f6f0ccac3df37af6886ee1d0f781a8bb6c
User <User@DESKTOP-34G779U>
2022-06-06 13:58:31 +0530

    first commit

---
+++ corejava/src/abc/demo.java
@@ -0,0 +1,26 @@
+package abc;
+
+ class A
+{
+	 void run()
+	{
+		
+	}
+}
+
+public class demo extends A {
+	final int speedlimit=90;//final variable  
+	void run(){  
+	speedlimit=90;  
+	 }  
+
+	public static void main(String[] args) {
+		demo obj=new demo ();  
+		obj.run();  
+		 }  
+		}
+
+		
+
+	
+
---
+++ corejava/src/new2/rbi.java
@@ -0,0 +1,10 @@
+package new2;
+
+public class rbi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
