873a30c357dc4ec7881d632db1ef3ebe428e19fa
User <User@DESKTOP-34G779U>
2022-06-08 21:40:19 +0530

    first commit

---
+++ corejava/src/Project/FinalDemo.java
@@ -0,0 +1,17 @@
+package Project;
+
+public class FinalDemo {
+	final int speedlimit=90;//Final variable
+	//void run() {
+		//speedlimit = 50;
+	//}
+//void run gives error cz Final value cannot be changed
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//FinalDemo fd = new FinalDemo();
+				//fd.run();
+
+	}
+
+}
