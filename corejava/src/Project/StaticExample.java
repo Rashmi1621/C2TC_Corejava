7a94ebe9b847d3d82f8f30a139a427c8997dc467
User <User@DESKTOP-34G779U>
2022-06-08 21:50:22 +0530

    first commit

---
+++ corejava/src/Project/StaticExample.java
@@ -0,0 +1,20 @@
+package Project;
+
+public class StaticExample {
+	static int a=10;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StaticExample s1 = new StaticExample();
+		StaticExample s2 = new StaticExample();
+		System.out.println("s1.a value "+s1.a);
+		System.out.println("s2.a value "+s2.a);
+		
+		s1.a = 20;
+		System.out.println("s1.a value "+s1.a);
+		System.out.println("s2.a value "+s2.a);
+
+
+	}
+
+}
