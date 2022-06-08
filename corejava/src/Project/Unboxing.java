047da64da86541504c6a9fff3fc73372a04ba4f4
User <User@DESKTOP-34G779U>
2022-06-08 21:54:19 +0530

    first commit

---
+++ corejava/src/Project/Unboxing.java
@@ -0,0 +1,14 @@
+package Project;
+
+public class Unboxing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Character ch = 'a';
+		char a = ch;
+		System.out.println("Character ch: "+ch);
+		System.out.println("char a: "+a);
+
+	}
+
+}
