b404bcec3bdb592ba42f409e77eb69e28cdd6f4c
User <User@DESKTOP-34G779U>
2022-06-09 15:17:56 +0530

    first commit

--- corejava/src/Project/student.java
+++ corejava/src/Project/student.java
@@ -1,13 +1,15 @@
 package Project;
+//Default Constructor 
 
 public class student {
-
-	public student() {
-		// TODO Auto-generated constructor stub
-	}
+	Student();{
+		System.out.println("Student object is created");
+		}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Student ob = new Student();
+
 
 	}
 
