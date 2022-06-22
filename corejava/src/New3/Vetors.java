52843dff74e9d3afd34f896972d97d0fe32873ca
User <User@DESKTOP-34G779U>
2022-06-22 06:38:50 +0530

    first commit

---
+++ corejava/src/New3/Vetors.java
@@ -0,0 +1,33 @@
+package New3;
+
+import java.util.Iterator;
+import java.util.Stack;
+
+
+public class Vetors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<String>stack = new Stack<String>();
+		stack.push("hi");
+		stack.push("hello");
+		stack.push("gm");
+		stack.push("gn");
+		//Iterartor for the stack
+		Iterator<String>itr=stack.iterator();
+	//printing the stack
+		while(itr.hasNext()) {
+			System.out.println(itr.next()+ "");
+		}
+		System.out.println();
+		stack.pop();
+		//iterator for the stack
+		itr=stack.iterator();
+		//printing the stack
+		while(itr.hasNext()) {
+			System.out.print(itr.next()+" ");
+		}
+
+	}
+
+}
