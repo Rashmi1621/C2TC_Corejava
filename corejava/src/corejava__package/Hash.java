6157ba938f3d580e42d3b2dc3ec9dbabaf22cbed
User <User@DESKTOP-34G779U>
2022-06-22 10:29:15 +0530

    first commit

---
+++ corejava/src/corejava__package/Hash.java
@@ -0,0 +1,19 @@
+package corejava__package;
+import java.util.*;
+import java.util.HashSet;
+
+
+public class Hash {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedHashSet<String>ins = new LinkedHashSet<String>();
+		HashSet<String> set = new HashSet<String> ();
+		set.add("Rashmi");
+		set.add("Samarth");
+		set.add("Teju");
+	  System.out.println(set);
+
+	}
+
+	}
