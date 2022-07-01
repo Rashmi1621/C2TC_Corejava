db410a4957f5f169644f575ef881f2d5c808ede4
User <User@DESKTOP-34G779U>
2022-07-01 10:37:40 +0530

    first commit

---
+++ corejava/src/thread/SleepThread.java
@@ -0,0 +1,34 @@
+package thread;
+
+public class SleepThread implements Runnable{
+	public void run()
+	{
+		System.out.println("good morning");
+		try
+		{
+			Thread.sleep(2000);
+			
+		}
+		catch(InterruptedException ie)
+		{
+			System.out.println(ie.getMessage());
+		}System.out.println("java");
+		System.out.println(Thread.currentThread());
+	}
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SleepThread a=new SleepThread();
+		Thread	t=new Thread(a,"child ");
+		t.start();
+		System.out.println("number of active "+Thread.activeCount());
+			a.m1();
+
+
+	}
+
+}
