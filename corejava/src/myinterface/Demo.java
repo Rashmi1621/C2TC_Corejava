94898b71a70682dbefbbdb0e8b24688e917533e1
User <User@DESKTOP-34G779U>
2022-07-14 01:53:50 +0530

    first commit

---
+++ corejava/src/myinterface/Demo.java
@@ -0,0 +1,13 @@
+package myinterface;
+class A
+{
+int x = 10;
+public void assign(int x)
+{
+x = x;
+System.out.println(this.x);
+}
+  	}
+
+
+				 	 																																																																																																																																																					
