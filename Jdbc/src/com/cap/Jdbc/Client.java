23e01bf758ccfe10cbc7e3191c9aa581cc4a08b1
User <User@DESKTOP-34G779U>
2022-08-08 11:33:17 +0530

    first commit

--- Jdbc/src/com/cap/Jdbc/Client.java
+++ Jdbc/src/com/cap/Jdbc/Client.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 
 public class Client {
 
-	public static void main(String[] args) throws SQLException{
+	public static void main(String[] args) throws SQLException{ 
 		// TODO Auto-generated method stub
 
 Connection connection =DBUtil.getconnection();
